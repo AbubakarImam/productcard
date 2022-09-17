c77078f14a9cdfbfbb125cf21207540758bd3ea3
AbubakarImam <salamabubakarimam@gmail.com>
2022-09-17 09:41:49 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '375px'
+    },
+    extend: {
+      colors: {
+        darkCyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        darkBlue: 'hsl(212, 21%, 14%)',
+        grayishBlue: 'hsl(228, 12%, 48%)',
+        White: 'hsl(0, 0%, 100%)'
+      } 
+    },
+  },
+  plugins: [],
+}
